721c4579d4223ced9f4aebedbfaf34ff0cd8d0ce
m&m <43050166+mischano@users.noreply.github.com>
2021-04-15 21:29:50 -0700

    Text Chat Added
    Text chat with server and clients using sockets added.

--- server.js
+++ server.js
@@ -1,12 +1,87 @@
+/*****************************************************
+ * npm run dev -- To run the server on local host.   *
+ * http://localhost:3000/                            *
+ ****************************************************/
+
 const express = require("express");
 const mongoose = require("mongoose");
+const socketio = require('socket.io');
+
 const profileRouter = require("./routes/profileRoutes.js");
 const Profile = require("./models/profile");
 const Class = require("./models/class");
 
-port = 3000
-
 const app = express();
+const path = require('path');
+const http = require('http');
+const server = http.createServer(app);  // create local server
+const io = socketio(server);    // connect socket to server
+const formatMessage = require('./utils/messages');
+const { userJoin, getCurrentUser, userLeave, getRoomUsers } = require('./utils/users');
+
+const PORT = 3000 || process.env.PORT;
+const botName = 'chat bot ';
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+/*
+ * All socket 'message' are caught in/from public -> js -> main.js *
+ */
+// Run when client connects to a chat room
+io.on('connection', socket => {     // listen for 'connection' event
+    socket.on('joinRoom', ({ username, room }) => {
+        // Send user data to utils -> users.js
+        const user = userJoin(socket.id, username, room); 
+        
+        socket.join(user.room); // join a specific room
+
+        // Emit to connected (single) user
+        socket.emit(
+            'message', 
+            formatMessage(botName, 'Welcome to chat')
+            );  
+
+        // Emit to everyone in a specific room
+        socket.broadcast
+            .to(user.room)
+            .emit(
+            'message', 
+            formatMessage(botName, `${user.username} has joined the chat`)
+            ); 
+    });
+
+    // Listen for chat message
+    socket.on('chatMessage', msg => {
+        // Get from utils -> users.js
+        const user = getCurrentUser(socket.id); 
+
+        // Emit to everyone in a specific room
+        io.to(user.room)
+            .emit(
+            'message', 
+            formatMessage(user.username, msg)
+            );
+    });
+    
+    // If client disconnects from chat room
+    // Emit to everyone
+    socket.on('disconnect', () => {  
+        // Get from utils -> users.js
+        const user = userLeave(socket.id);
+
+        // Emit to everyone in a specific room
+        if (user) {
+            io.to(user.room)
+                .emit(
+                'message', 
+                formatMessage(botName, `${user.username} has left the chat`)
+                );
+        }
+    });
+});
+
+server.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
 
 mongoose.connect(
    "mongodb+srv://StudyConnectUser:srcAVv$vq!7Lvfr@studyconnect.dscne.mongodb.net/StudyConnect?retryWrites=true&w=majority",
@@ -15,7 +90,6 @@ mongoose.connect(
       useFindAndModify: false,
       useUnifiedTopology: true
    });
-
 
 /*
 const classData = {
@@ -52,7 +126,7 @@ app.get('/', (req, res) => {
    res.send("Hello world!")
 });
 
-app.listen(port, () => {
+app.listen(PORT, () => {
    console.log('Server is running...')
 });
 */
