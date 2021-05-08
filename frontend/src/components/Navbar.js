890d576284598b26f22ee4bbd5d2034ff57b8d6b
Caroline <cpadil12@calpoly.edu>
2021-05-07 22:59:27 -0700

    Changed navbar

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,87 @@
+import React, { useState, useEffect } from 'react';
+import '../App.css';
+import { Link, useHistory, useLocation } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { Button } from '@material-ui/core';
+import decode from 'jwt-decode';
+import {
+   Nav, 
+   NavLink, 
+   Bars, 
+   NavMenu, 
+   NavBtn, 
+   NavBtnLink
+} from './NavBarElements';
+
+const Navbar = () => {
+   const navStyle = {color: 'black'};
+   const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
+   const dispatch = useDispatch();
+   const history = useHistory();
+   const location = useLocation();
+
+   const logout = () => {
+      dispatch({ type: 'LOGOUT' });
+      history.push('/');
+      setUser(null);
+   }
+
+   useEffect(() => {
+      const token = user?.token;
+
+      //logouts the user if its been more than an hour
+      if(token) {
+         const decodedToken = decode(token);
+
+         if (decodedToken.exp * 1000 < new Date().getTime()) logout();
+      }
+
+      setUser(JSON.parse(localStorage.getItem('profile')))
+      }, [location])
+
+   return (
+      <>
+      <Nav>
+         <NavLink to="/">
+               <h1>Logo</h1>
+               {/*/*</NavLink><img src={require('./Assets/logo.svg')}*/}
+         </NavLink>
+         <Bars /> 
+         <NavMenu>
+            <NavLink to="/" activeStyle> 
+               Dashboard
+            </NavLink>
+            <NavLink to="/groups" activeStyle> 
+               Groups
+            </NavLink>
+            <NavLink to="/messages" activeStyle> 
+               Messages
+            </NavLink>
+            <NavLink to="/profile" activeStyle> 
+               {user ? (
+                  user.result.name
+                  ) : null}
+            </NavLink>
+            <NavLink to="/signup1" activeStyle>
+               Signup 1
+            </NavLink>
+            <NavLink to="/signup2" activeStyle>
+               Signup 2
+            </NavLink>
+            <NavLink to="/signup3" activeStyle>
+               Signup 3
+            </NavLink>
+         </NavMenu>
+         <NavBtn>
+            <NavBtnLink to="/auth">Login</NavBtnLink>
+         </NavBtn>
+         <Button onClick={logout}>
+            Logout
+         </Button>
+      </Nav>
+      </>
+   );
+}
+
+
+export default Navbar
