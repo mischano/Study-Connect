fe8e4aaa73f9bebe54d723de95016b19c608d130
m&m <43050166+mischano@users.noreply.github.com>
2021-05-21 23:21:21 -0700

    seach bar completed
    basics of a searchbar is completed. need to work on styling and some extra futures. Will do it after doing friends...(?)

--- frontend/src/components/Profile/FriendsList.js
+++ frontend/src/components/Profile/FriendsList.js
@@ -1,43 +1,62 @@
-import { Grid, makeStyles } from '@material-ui/core';
-import React from 'react';
+import { Grid, Button, makeStyles } from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
 import mongoose from 'mongoose';
 import '../../App.css';
 import { getUser } from '../../actions/auth';
+import Box from '@material-ui/core/Box'
 
 function fetchUser() {
-   if (JSON.parse(localStorage.getItem('profile'))) {
-      let user = (JSON.parse(localStorage.getItem('profile'))).result;
-      return user;
-   } else {
-      return null;
-   }
+    if (JSON.parse(localStorage.getItem('profile'))) {
+        let user = (JSON.parse(localStorage.getItem('profile'))).result;
+        return user;
+    } else {
+        return null;
+    }
 }
 function fetchFriends() {
-   let user = fetchUser();
-   if (user) {
-      let friends = user.friends;
-      return friends;
-   }
+    let user = fetchUser();
+    if (user) {
+        let friends = user.friends;
+        return friends;
+    }
 }
 const useStyles = makeStyles((theme) => ({
 
 }));
 
 const FriendsList = () => {
-   const friends = useStyles();
-   let user = fetchUser();
-   fetchFriends();
+    const friends = useStyles();
+    let user = fetchUser();
+    const [users, setUsers] = useState([]);
 
-   return (
-      <Grid className="friends">
-         <h2 className="sectionHeader">Your Friends</h2>
-         <Grid container spacing={4} direction={'column'} justify="space-evenly">
-            {user.friends.map(friend => (
-               <h3>{getUser(friend).name}</h3>
-            ))}
-         </Grid>
-      </Grid>
-   );
+    const getFriends = async () => {
+        user.friends.map(async friend => {
+            await getUser(friend).then(res => {
+                setUsers(users.concat(res.name))
+            })
+        })
+    }
+
+    useEffect(() => {
+        getFriends();
+    }, [])
+
+    fetchFriends();
+
+    const reptiles = ["alligator", "snake", "lizard"];
+
+    const PrintStuff = () => {
+        console.log(users);
+    }
+        // <ol>
+        //     {reptiles.map((reptile) => (
+        //         <li>{reptile}</li>
+        //     ))}
+        // </ol>
+
+    return (
+        console.log(users)
+    );
 }
 
 export default FriendsList;
