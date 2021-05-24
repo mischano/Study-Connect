947609679d4b244c1575251a3fd375298192553e
Lradovan <lucasradovan@gmail.com>
2021-05-23 18:13:59 -0700

    groups now display members names

--- frontend/src/components/Groups/Group.js
+++ frontend/src/components/Groups/Group.js
@@ -1,28 +1,41 @@
 import React, { useEffect, useState } from 'react';
 import '../../App.css';
 import { getGroup } from '../../actions/group';
+import { getUser } from '../../actions/auth';
 import { Link } from 'react-router-dom';
 
 const Group = ({ match }) => {
    const [group, setGroup] = useState(null);
+   const [members, setMembers] = useState([]);
 
    const fetchGroup = async () => {
-      const user = await getGroup(match.params.id);
-      setGroup(user);
+      const g = await getGroup(match.params.id);
+      setGroup(g);
    }
-
+ 
    useEffect(() => {
-      fetchGroup();
-   });
+      fetchGroup()
+   }, []);
+
+   //gets the members of the groups from database
+   const getMembers = async () => {
+    Promise.all(group.members.map(async mem => {
+       return getUser(mem);
+    })).then(arr => setMembers(mems => [...mems, ...arr]))
+ }
+
+ // call get members
+ if(group !== null && group.members.length !== 0 && members.length === 0)
+    getMembers();
 
    return (
       <div>
          {group && (
             <>
                <h1> {group.name} </h1>
-               {group.members.map((friend, i) => {
+               {members.map((mem, i) => {
                   return <li key={i}>
-                     <Link to={`/profile/${friend}`} key={friend._id}>{friend}</Link>
+                     <Link to={`/profile/${mem._id}`} key={mem._id}>{mem.name}</Link>
                   </li>
                })}
             </>
