ab7dc459e39b818674c221d1ace6d4f7b50c2916
Raphael Pham <raphaelpham14@gmail.com>
2021-05-28 21:18:10 -0700

    Worked on friend reqs

--- frontend/src/components/Navbar/Notif.js
+++ frontend/src/components/Navbar/Notif.js
@@ -47,8 +47,9 @@ const Notif = () => {
    }, []);
 
    const fetchFriendReqs = async () => {
-      const allReqs = await getFriendReqs(fetchUser()._id).then(res => console.log(res));
-      setReqs(allReqs);
+      await getFriendReqs(fetchUser()._id)
+      .then(allReqs => setReqs(allReqs))
+      .then(res => console.log(res));
    }
 
    return (
@@ -69,14 +70,6 @@ const Notif = () => {
                   ref={descriptionElementRef}
                   tabIndex={-1}
                >
-                  {[...new Array(50)]
-                     .map(
-                        () => `Cras mattis consectetur purus sit amet fermentum.
-Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
-                     )
-                     .join('\n')}
                </DialogContentText>
 
             </DialogContent>
