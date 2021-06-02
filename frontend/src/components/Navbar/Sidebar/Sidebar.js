933e4630f768bd10315c892c109c15937b5b08b7
Caroline <cpadil12@calpoly.edu>
2021-06-02 00:35:55 -0700

    Added hamburger menu functionality

---
+++ frontend/src/components/Navbar/Sidebar/Sidebar.js
@@ -0,0 +1,49 @@
+import React, {useState} from 'react'
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import { Link } from 'react-router-dom';
+import { SidebarData } from './SidebarData'; 
+import './Sidebar.css';
+import { IconContext } from 'react-icons'
+
+function Sidebar() {
+    const [sidebar, setSidebar] = useState(false);
+
+    const showSidebar = () => setSidebar(!sidebar);
+
+    return (
+        <>
+        <IconContext.Provider value={{color: '#006494'}}>
+            <div className="sidebar">
+                <Link to='#' className='menu-bars'>
+                    <FaIcons.FaBars onClick={showSidebar} />
+                </Link>
+            </div>
+        </IconContext.Provider> 
+
+            <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+                <ul className= 'nav-menu-items' onClick={showSidebar}>
+                    <li className= 'navbar-toggle' >
+                    <IconContext.Provider value={{color: '#E67350'}}>
+                        <Link to='#' className='menu-bars'>
+                            <AiIcons.AiOutlineClose />
+                        </Link>
+                    </IconContext.Provider>
+                    </li>
+                    {SidebarData.map((item, index) => {
+                        return ( 
+                            <li key={index} className={item.cName}>
+                                <Link to={item.path}>
+                                    {item.title}
+                                </Link>
+                            </li>
+                        )
+                    })}
+                        
+                </ul>
+                </nav>          
+        </>
+    );
+}
+
+export default Sidebar
---
+++ frontend/src/components/Navbar/Sidebar/SidebarData.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import * as IoIcons from "react-icons/io";
+
+export const SidebarData = [ 
+    {
+        title: 'Dashboard',
+        path: '/dashboard', 
+        cName: 'nav-text'
+    },
+    {
+        title: 'Groups',
+        path: '/groups', 
+        cName: 'nav-text'
+    },
+    {
+        title: 'Profile',
+        path: '/profile', 
+        cName: 'nav-text'
+    }
+]
