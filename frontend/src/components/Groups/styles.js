bf17dde3cc386fb1c6d965a0ce8e26ffc89cb586
Caroline <cpadil12@calpoly.edu>
2021-06-04 01:48:43 -0700

    styled group page

--- frontend/src/components/Groups/styles.js
+++ frontend/src/components/Groups/styles.js
@@ -8,6 +8,7 @@ export default makeStyles((theme) => ({
         alignItems: 'left',
         padding: theme.spacing(2),
         fontFamily: 'Lato',
+        width: '100%',
     },
 
     heading:{ 
@@ -28,6 +29,8 @@ export default makeStyles((theme) => ({
 
     field: {
         borderRadius: 25,
+        width: "100%", 
+        // padding: theme.spacing(2),
     },
 
     submit: {
@@ -48,15 +51,16 @@ export default makeStyles((theme) => ({
     },
 
     invite: {
-        margin: theme.spacing(3, 0, 2),
+        margin: theme.spacing(2, 2, 2, 5),
         border: 0,
         borderRadius: 40,
         boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
         background: '#E67350',
         color: 'white',
         height: 48,
-        width: '10%',
+        padding: theme.spacing(0, 3),
         fontWeight: 'Bold',
+
 
         '&:hover': { 
             backgroundColor: '#E6592E',
@@ -64,9 +68,32 @@ export default makeStyles((theme) => ({
         },
     },
 
+    leave: { 
+        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+        height: 48,
+        fontWeight: 'Bold',
+        margin: theme.spacing(2, 0, 2, 5),
+        padding: theme.spacing(0, 3),
+        backgroundColor: "#fff",
+        border: "2px solid",
+        borderColor: "#E67350",
+        borderRadius: 40,
 
+        '&:hover': { 
+            backgroundColor: '#E6592E',
+            boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+        },
+    }, 
+
+    members: { 
+        marginTop: theme.spacing(30),
+    },
 
     h4: {
         marginBottom: theme.spacing(3),
+    },
+
+    schedule: { 
+
     }
 }));
